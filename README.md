2b4444f6a8b313cbb9a89992263b3ccdb4f68734
Lukasz Barcinski <lukaszb.gwz@gmail.com>
2026-01-17 17:58:11 +0100

    Prepare first launch instruction and script

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ Project consists of subprojects:
 - **backend** - REST webservice written in RUST, which serves requests from `web-admin` and `web-user` and optionaly other clients.
 - **database** - all files and scripts concerning database.
 
+## First run
+
+1. Install docker and rust (with cargo) in version at least 1.70
+2. Set environment variables (if not set, default ones will be used):
+* `CA_PASSWORD` - password for openssl CA private key
+* `JWT_SECRET` - password for JWT (Json-Web-Token) mechanism
+* `DATABASE_URL` - url and password for  database connection
+3. Execute `./prepare_environment.sh`
+4. Start application with `docker-compose up` (or `docker compose up`)
--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# 2. Sprawdzenie 'docker-compose' (V1 lub samodzielny binarek V2)
+if command -v docker-compose >/dev/null 2>&1; then
+  DOCKER_COMPOSE="docker-compose"
+else
+  if docker compose version >/dev/null 2>&1; then
+    DOCKER_COMPOSE="docker compose"
+  fi
+fi
+
+if [ "$DOCKER_COMPOSE" == "" ]; then
+  echo "docker-compose not found"
+  exit -1
+fi
+echo "Found ${DOCKER_COMPOSE}"
+
 if [ "$CA_PASWORD" == "" ]; then
   export CA_PASSWORD="ca_password"
 fi
@@ -26,7 +41,11 @@ if [ ! -d ca ]; then
   mkdir ca
   openssl genrsa -aes256 -passout pass:$CA_PASSWORD -out ca/ca.key 2048
   openssl req -x509 -new -nodes -key ca/ca.key -passin env:CA_PASSWORD -sha256 -days 3650 -out ca/ca.crt -subj "/CN=KeyInfrastructureCA"
+else
+  echo "ca directory already exists"
 fi
+
+cargo build || exit -1
 
 cd ../frontend
 
@@ -34,3 +53,5 @@ if [ ! -n .env ]; then
   echo "Creating frontend .env"
   cp .env.example .env
 fi
+
+$DOCKER_COMPOSE build
